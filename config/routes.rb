650ad4dbf203cddb6ec2ad05effaf5cee37dbb26
Nick Don <nick.don.1102@gmail.com>
2022-06-09 18:54:58 +1000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :restaurants do
-    resources :reviews, only: [:new, :create]
-  end
-  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
